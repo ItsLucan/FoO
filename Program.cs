cde11f64ffb0d9d4cfb2c9bc692ffb65352cbc25
Lucan <imlucan26@gmail.com>
2026-08-02 16:44:07 -0400

    Reworked Player parameters and movement

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿Cave cave = new Cave(4, 4);
-Player player = new Player(cave.Rooms.GetLength(0), cave.Rooms.GetLength(1));
+Player player = new Player();
 Game game = new Game(player, cave);
 
 game.Run();
@@ -22,14 +22,14 @@ public class Game
         while (true)
         {
             Console.Clear();
-            UpdatePlayerPosition();
+            UpdatePlayerRoom();
             Display();
             Console.WriteLine(_player.Location);
-            _player.Move();
+            MovePlayer();
         }
     }
 
-    private void UpdatePlayerPosition()
+    private void UpdatePlayerRoom()
     {
         if (!_player.Location.Equals(_currentRoom.Location))
         {
@@ -40,6 +40,22 @@ public class Game
         _currentRoom.SetPlayerHere(true);
     }
 
+    private void MovePlayer()
+    {
+        Location desiredLocation = new Location(_player.Location.Row, _player.Location.Column);
+        
+        desiredLocation = _player.GetInput() switch
+        {
+            ConsoleKey.W when _player.Location.Row - 1 >= 0 => _player.Location with { Row = _player.Location.Row - 1 },
+            ConsoleKey.A when _player.Location.Column - 1 >= 0 => _player.Location with { Column = _player.Location.Column - 1 },
+            ConsoleKey.S when _player.Location.Row + 1 < _cave.Rows => _player.Location with { Row = _player.Location.Row + 1 },
+            ConsoleKey.D when _player.Location.Column + 1 < _cave.Columns => _player.Location with { Column = _player.Location.Column + 1 },
+            _ => _player.Location
+        };
+        
+        _player.UpdateLocation(desiredLocation);
+    }
+    
     private void Display()
     {
         for (int row = 0; row < _cave.Rooms.GetLength(0); row++)
@@ -54,23 +70,15 @@ public class Game
     }
 }
 
-public class Player(int caveRows, int caveColumns)
+public class Player()
 {
-    private int _caveRows = caveRows;
-    private int _caveColumns = caveColumns;
     public Location Location { get; private set; } = new Location { Row = 0, Column = 0};
-    // TODO find a way to remove magic numbers from movement switch
-    public void Move()
+    
+    public ConsoleKey GetInput() => Console.ReadKey(true).Key;
+
+    public void UpdateLocation(Location location)
     {
-        ConsoleKey input = Console.ReadKey(true).Key;
-        Location = input switch
-        { 
-            ConsoleKey.W when Location.Row - 1 >= 0 => Location with { Row = Location.Row - 1 },
-            ConsoleKey.A when Location.Column - 1 >= 0 => Location with { Column = Location.Column - 1 },
-            ConsoleKey.S when Location.Row + 1 < _caveRows => Location with { Row = Location.Row + 1 },
-            ConsoleKey.D when Location.Column + 1 < _caveColumns => Location with { Column = Location.Column + 1 },
-            _ => Location
-        };
+        Location = location;
     }
 }
 
@@ -82,10 +90,8 @@ public class Cave
     
     public Cave(int rows, int columns)
     {
-        // TODO does this make sense to have Row/Column properties so player knows movelimit bounds? Should player know that?
         Rows = rows;
         Columns = columns;
-        
         Rooms = new Room[Rows, Columns];
         
         for (int row = 0; row < Rooms.GetLength(0); row++)
@@ -99,10 +105,7 @@ public class Cave
         // TODO possible implementation of different Rooms? Rooms[0, 0] = new Room(RoomType.Entrance, new Location(0, 0));
     }
 
-    public Room GetRoomAt(Location location)
-    {
-        return Rooms[location.Row, location.Column];
-    }
+    public Room GetRoomAt(Location location) => Rooms[location.Row, location.Column];
 }
 
 public class Room(RoomType type, Location location)
--- obj/Debug/net10.0/The Fountain of Objects.AssemblyInfo.cs
+++ obj/Debug/net10.0/The Fountain of Objects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The Fountain of Objects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0758c087faedffdf62e5e8e0be15a41e543cffce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d039aa7442cae3e74531840dbbb16926023ed23a")]
 [assembly: System.Reflection.AssemblyProductAttribute("The Fountain of Objects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The Fountain of Objects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
