cb1e5bc93a32b778263ee622f0ac6901610ccb2e
Lucan <imlucan26@gmail.com>
2026-08-02 16:56:39 -0400

    Background stuff I think?

--- obj/Debug/net10.0/The Fountain of Objects.AssemblyInfo.cs
+++ obj/Debug/net10.0/The Fountain of Objects.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The Fountain of Objects")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d039aa7442cae3e74531840dbbb16926023ed23a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31d3049100694fbbeb07332da9be6e2603819c0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("The Fountain of Objects")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The Fountain of Objects")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
