5955b735b5c8867982b046baa77060a26b08e5c7
Lucan <imlucan26@gmail.com>
2026-08-10 20:23:13 -0400

    made sensor fields constant

--- Scripts/Sensor.cs
+++ Scripts/Sensor.cs
@@ -2,13 +2,13 @@ namespace The_Fountain_of_Objects.Scripts;
 
 public class Sensor()
 {
-    private readonly string _seeColor      = "\e[38;2;251;245;43m";
-    private readonly string _hearColor     = "\e[38;2;137;251;43m";
-    private readonly string _feelColor     = "\e[38;2;194;49;160m";
-    private readonly string _smellColor    = "\e[38;2;251;159;43m";
-    private readonly string _negativeColor = "\e[38;2;172;48;0m";
-    private readonly string _fountainColor = "\e[38;2;60;120;255m";
-    private readonly string _resetColor    = "\e[39m";
+    private const string SeeColor      = "\e[38;2;251;245;43m";
+    private const string HearColor     = "\e[38;2;137;251;43m";
+    private const string FeelColor     = "\e[38;2;194;49;160m";
+    private const string SmellColor    = "\e[38;2;251;159;43m";
+    private const string NegativeColor = "\e[38;2;172;48;0m";
+    private const string FountainColor = "\e[38;2;60;120;255m";
+    private const string ResetColor    = "\e[39m";
     
     public void DisplayCurrentSense(Room currentRoom, bool isFountainRepaired)
     {
@@ -32,19 +32,19 @@ public class Sensor()
         
         string? currentRoomText = currentRoom.RoomType switch
         {
-            RoomType.Entrance when isFountainRepaired  => $"{_feelColor}FEEL:{_resetColor} The warm embrace of the free sun through the caves entrance. You have conquered The Uncoded Ones challenge.", 
-            RoomType.Fountain when isFountainRepaired  => $"{_hearColor}HEAR:{_resetColor} Water rushing from the {_fountainColor}Fountain of objects{_resetColor}. It is repaired.",
-            RoomType.Fountain                          => $"{_seeColor}SEE:{_resetColor} The silhouette of a large {_fountainColor}fountain{_resetColor}. You are in the fountain room.",
-            RoomType.Entrance                          => $"{_seeColor}SEE:{_resetColor} light from outside the cave. You are at the entrance.",
-            RoomType.Pit                               => $"{_negativeColor}GAME OVER:{_resetColor} You step onto ground with no substance, and tumble into a vast chasm. You died.",
+            RoomType.Entrance when isFountainRepaired  => $"{FeelColor}FEEL:{ResetColor} The warm embrace of the free sun through the caves entrance. You have conquered The Uncoded Ones challenge.", 
+            RoomType.Fountain when isFountainRepaired  => $"{HearColor}HEAR:{ResetColor} Water rushing from the {FountainColor}Fountain of objects{ResetColor}. It is repaired.",
+            RoomType.Fountain                          => $"{SeeColor}SEE:{ResetColor} The silhouette of a large {FountainColor}fountain{ResetColor}. You are in the fountain room.",
+            RoomType.Entrance                          => $"{SeeColor}SEE:{ResetColor} light from outside the cave. You are at the entrance.",
+            RoomType.Pit                               => $"{NegativeColor}GAME OVER:{ResetColor} You step onto ground with no substance, and tumble into a vast chasm. You died.",
             RoomType.Empty                             => null,
             _                                          => "ERROR: CURRENT ROOM UNACCOUNTED FOR."
         };
 
         string? currentEnemyText = currentRoom.EnemyType switch
         {
-            EnemyType.Maelstrom => $"{_feelColor}FEEL:{_resetColor} The torrential strength of a Maelstrom. You both are sent flying through the cave.",
-            EnemyType.Amarok    => $"{_negativeColor}GAME OVER:{_resetColor} You waltz into the maw of a foul Amarok, who rends your flesh. You have died.",
+            EnemyType.Maelstrom => $"{FeelColor}FEEL:{ResetColor} The torrential strength of a Maelstrom. You both are sent flying through the cave.",
+            EnemyType.Amarok    => $"{NegativeColor}GAME OVER:{ResetColor} You waltz into the maw of a foul Amarok, who rends your flesh. You have died.",
             EnemyType.None      => null,
             _                   => "ERROR: CURRENT ENEMY UNACCOUNTED FOR."
         };
@@ -56,16 +56,16 @@ public class Sensor()
     {
         string? adjacentRoomText = adjacentRoom.RoomType switch
         {
-            RoomType.Fountain                   => $"{_hearColor}HEAR:{_resetColor} A faint dripping in the distance. The {_fountainColor}fountain{_resetColor} is close.",
-            RoomType.Pit                        => $"{_feelColor}FEEL:{_resetColor} The howling breath of a hungry chasm. A {_negativeColor}pit{_resetColor} is nearby.",
+            RoomType.Fountain                   => $"{HearColor}HEAR:{ResetColor} A faint dripping in the distance. The {FountainColor}fountain{ResetColor} is close.",
+            RoomType.Pit                        => $"{FeelColor}FEEL:{ResetColor} The howling breath of a hungry chasm. A {NegativeColor}pit{ResetColor} is nearby.",
             RoomType.Entrance or RoomType.Empty => null,    
             _                                   => "ERROR: ADJACENT ROOM UNACCOUNTED FOR."
         };
 
         string? adjacentEnemyText = adjacentRoom.EnemyType switch
         {
-            EnemyType.Maelstrom => $"{_feelColor}FEEL:{_resetColor} The ghastly winds of a {_negativeColor}Maelstrom{_resetColor} nearby.",
-            EnemyType.Amarok    => $"{_smellColor}SMELL:{_resetColor} The pungent odor of rotten flesh. An {_negativeColor}Amarok{_resetColor} is nearby.",
+            EnemyType.Maelstrom => $"{FeelColor}FEEL:{ResetColor} The ghastly winds of a {NegativeColor}Maelstrom{ResetColor} nearby.",
+            EnemyType.Amarok    => $"{SmellColor}SMELL:{ResetColor} The pungent odor of rotten flesh. An {NegativeColor}Amarok{ResetColor} is nearby.",
             EnemyType.None      => null,
             _                   => "ERROR: ADJACENT ENEMY UNACCOUNTED FOR."
             
