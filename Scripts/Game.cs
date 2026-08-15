647e88fd73e7cb9adfb7895d2e4c40805ba4aaed
Lucan <imlucan26@gmail.com>
2026-08-15 12:32:30 -0400

    Refactoring colors

--- Scripts/Game.cs
+++ Scripts/Game.cs
@@ -2,10 +2,10 @@ namespace The_Fountain_of_Objects.Scripts;
 
 public class Game
 {
-    private const string SeeColor      = "\e[38;2;251;245;43m";
-    private const string HearColor     = "\e[38;2;137;251;43m";
-    private const string FeelColor     = "\e[38;2;194;49;160m";
-    private const string SmellColor    = "\e[38;2;251;159;43m";
+    private const string See           = $"\e[38;2;251;245;43mSEE:{ResetColor}";
+    private const string Hear          = $"\e[38;2;137;251;43mHEAR:{ResetColor}";
+    private const string Feel          = $"\e[38;2;194;49;160mFEEL:{ResetColor}";
+    private const string Smell         = $"\e[38;2;251;159;43mSMELL:{ResetColor}";
     private const string NegativeColor = "\e[38;2;172;48;0m";
     private const string FountainColor = "\e[38;2;60;120;255m";
     private const string ResetColor    = "\e[39m";
@@ -119,10 +119,10 @@ public class Game
     {
         string? currentRoomText = _currentRoom.RoomType switch
         {
-            RoomType.Entrance when _isFountainRepaired  => $"{FeelColor}FEEL:{ResetColor} The warm embrace of the free sun through the caves entrance. You have conquered The Uncoded Ones challenge.\n\nPress any key to quit.", 
-            RoomType.Fountain when _isFountainRepaired  => $"{HearColor}HEAR:{ResetColor} Water rushing from the {FountainColor}Fountain of objects{ResetColor}. It is repaired.",
-            RoomType.Fountain                          => $"{SeeColor}SEE:{ResetColor} The silhouette of a large {FountainColor}fountain{ResetColor}. You are in the fountain room.",
-            RoomType.Entrance                          => $"{SeeColor}SEE:{ResetColor} light from outside the cave. You are at the entrance.",
+            RoomType.Entrance when _isFountainRepaired  => $"{Feel} The warm embrace of the free sun through the caves entrance. You have conquered The Uncoded Ones challenge.\n\nPress any key to quit.", 
+            RoomType.Fountain when _isFountainRepaired  => $"{Hear} Water rushing from the {FountainColor}Fountain of objects{ResetColor}. It is repaired.",
+            RoomType.Fountain                          => $"{See} The silhouette of a large {FountainColor}fountain{ResetColor}. You are in the fountain room.",
+            RoomType.Entrance                          => $"{See} light from outside the cave. You are at the entrance.",
             RoomType.Pit                               => $"{NegativeColor}GAME OVER:{ResetColor} You step onto ground with no substance, and tumble into a vast chasm.\n\nPress any key to quit.",
             RoomType.Empty                             => null,
             _                                          => "ERROR: CURRENT ROOM UNACCOUNTED FOR."
@@ -135,7 +135,7 @@ public class Game
     {
         string? currentEnemyText = _currentRoom.EnemyType switch
         {
-            EnemyType.Maelstrom => $"{FeelColor}FEEL:{ResetColor} The torrential strength of a {NegativeColor}Maelstrom{ResetColor}. You both are sent flying through the cave.\n\nPress any key to continue.",
+            EnemyType.Maelstrom => $"{Feel} The torrential strength of a {NegativeColor}Maelstrom{ResetColor}. You both are sent flying through the cave.\n\nPress any key to continue.",
             EnemyType.Amarok    => $"{NegativeColor}GAME OVER:{ResetColor} You waltz into the maw of a foul {NegativeColor}Amarok{ResetColor}, who rends your flesh. You have died.\n\nPress any key to quit.",
             EnemyType.None      => null,
             _                   => "ERROR: CURRENT ENEMY UNACCOUNTED FOR."
@@ -148,8 +148,8 @@ public class Game
     {
         string? adjacentRoomText = adjacentRoom.RoomType switch
         {
-            RoomType.Fountain                   => $"{HearColor}HEAR:{ResetColor} A faint dripping in the distance. The {FountainColor}fountain{ResetColor} is close.",
-            RoomType.Pit                        => $"{FeelColor}FEEL:{ResetColor} The howling breath of a hungry chasm. A {NegativeColor}pit{ResetColor} is nearby.",
+            RoomType.Fountain                   => $"{Hear} A faint dripping in the distance. The {FountainColor}fountain{ResetColor} is close.",
+            RoomType.Pit                        => $"{Feel} The howling breath of a hungry chasm. A {NegativeColor}pit{ResetColor} is nearby.",
             RoomType.Entrance or RoomType.Empty => null,    
             _                                   => "ERROR: ADJACENT ROOM UNACCOUNTED FOR."
         };
@@ -161,8 +161,8 @@ public class Game
     {
         string? adjacentEnemyText = adjacentRoom.EnemyType switch
         {
-            EnemyType.Maelstrom => $"{FeelColor}FEEL:{ResetColor} The ghastly winds of a {NegativeColor}Maelstrom{ResetColor} nearby.",
-            EnemyType.Amarok    => $"{SmellColor}SMELL:{ResetColor} The pungent odor of rotten flesh. An {NegativeColor}Amarok{ResetColor} is nearby.",
+            EnemyType.Maelstrom => $"{Feel} The ghastly winds of a {NegativeColor}Maelstrom{ResetColor} nearby.",
+            EnemyType.Amarok    => $"{Smell} The pungent odor of rotten flesh. An {NegativeColor}Amarok{ResetColor} is nearby.",
             EnemyType.None      => null,
             _                   => "ERROR: ADJACENT ENEMY UNACCOUNTED FOR."
             
